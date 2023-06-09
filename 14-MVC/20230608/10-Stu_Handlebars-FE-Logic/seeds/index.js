ed1ff0e43033b0612183e6881f70b3bcf32f5fa4
SV <72838881+santis1001@users.noreply.github.com>
2023-06-09 13:51:02 -0600

    end of class commit - 20230608

---
+++ 14-MVC/20230608/10-Stu_Handlebars-FE-Logic/seeds/index.js
@@ -0,0 +1,17 @@
+const sequelize = require('../config/connection');
+const Dish = require('../models/Dish');
+const dishData = require('./dish-seeds.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  console.log(dishData);
+  await Dish.bulkCreate(dishData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
