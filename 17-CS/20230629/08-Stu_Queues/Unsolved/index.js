83c7aacd1b6086c9a96a11e14b87ea514e9e0577
SV <72838881+santis1001@users.noreply.github.com>
2023-06-29 22:27:20 -0600

    end of class commit - 20230629

---
+++ 17-CS/20230629/08-Stu_Queues/Unsolved/index.js
@@ -0,0 +1,18 @@
+// create the Queue class with two methods to remove and add an element
+class Queue {
+    constructor(container = []) {
+        this.container = container;
+    }
+
+    // adds an element to the back of the queue
+    addToQueue(el) {
+        return this.container.push(el);
+    }
+
+    // removes an element from the front of the queue
+    removeFromQueue() {
+        return this.container.shift();
+    }
+}
+
+module.exports = Queue;
